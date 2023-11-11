f0845f4fe94b39fbfebc7a9fe0297a41e8099f75
Brien-Austin <brienaustinclayton@gmail.com>
2023-11-11 10:53:32 +0530

    Created Home Screen

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+const {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+} = require("react-native");
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.homeContent}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.head}>Cookzee</Text>
+        <Text style={styles.subHead}>cooking made easier</Text>
+      </View>
+      <View style={styles.btnContainer}>
+        <TouchableOpacity style={styles.login}>
+          <Text style={styles.btnText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.register}>
+          <Text style={styles.btnText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
+  homeContent: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  titleContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  head: {
+    fontSize: 36,
+    color: "#FBBB00",
+    fontWeight: "bold",
+  },
+  subHead: {
+    fontSize: 18,
+  },
+  btnContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20,
+    paddingBottom: 20,
+  },
+
+  login: {
+    backgroundColor: "#FBBB00",
+    height: 50,
+    borderRadius: 8,
+    width: 280,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  register: {
+    borderColor: "#FBBB00",
+    borderWidth: 1,
+    height: 50,
+    borderRadius: 8,
+    width: 280,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnText: {
+    fontSize: 18,
   },
 });
